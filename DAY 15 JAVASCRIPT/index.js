a5227f168453c0b06a8b6f4e3e8bba7b548a4e6b
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-03-26 20:29:32 +0530

    files updated

--- DAY 15 JAVASCRIPT/index.js
+++ DAY 15 JAVASCRIPT/index.js
@@ -1,9 +1,9 @@
-console.log("Hello");
-console.log("Welcome to my github repo.")
-console.log("I like pizza!");
+//console.log("Hello");
+//console.log("Welcome to my github repo.")
+//console.log("I like pizza!");
 
-window.alert("This is an alert!");
-window.alert("I like pizza!");
+//window.alert("This is an alert!");
+//window.alert("I like pizza!");
 
 //this is an comment
 
@@ -12,4 +12,97 @@ is
 also 
 a 
 comment */
+
+//document.getElementById("myh1").textContent='Hello';
+//document.getElementById("myp").textContent='How are You?//';
+
+//VARIABLE = a container that stores a value.
+//            Behaves as if it were the valur it contains
+
+// 1.Declration   let x;
+// 2.Assignment   x=299;
+
+/*let x;
+x=100;
+console.log(x) */
+
+//DATA TYPE 
+
+/*let price=34.56;
+let age=23;
+let gpa=3.3;
+
+console.log(`you are ${age} years of old `);
+console.log(`the price is $ ${price}`);
+console.log(`your gpa is ${gpa}`)
+
+console.log(typeof(age));
+console.log(typeof(price));*/
+
+//STRING : a series of character
+
+/*let firstname="Abhi";
+let favouritefood="Pizza";
+let email="abhi553@gmail.com";
+
+console.log(typeof(firstname))
+console.log(`your name is ${firstname}`);
+console.log(`you like ${favouritefood}`);
+console.log(`your email is ${email}`);*/
+
+//BOOLEAN
+
+/*let online=true;
+console.log(typeof(online));
+console.log(`Abhi is online ${online}`)
+
+let forsale=false;
+console.log(typeof(forsale));*/
+
+
+/*let fullname="abhishek kumar";
+let age=18;
+let isStudent=false;
+
+//document.getElementById("p1").textContent=fullname;
+//document.getElementById("p2").textContent=age;
+//document.getElementById("p3").textContent=isStudent;
+
+document.getElementById("p1").textContent=`your name is ${fullname}`;
+document.getElementById("p2").textContent=`you are ${age} years old`;
+document.getElementById("p3").textContent=`enrolled :${isStudent}`*/
+
+
+
+//string operation 
+
+
+//1.CONCATENATION
+
+//alert("hello"+" "+"brother");
+
+//challenge
+
+//var message="Hello"
+//var name="Abhishek."
+// output :hello there, Abhishek.
+
+//alert(message+ " "+"there," +  name);
+
+
+
+// 2.String length
+
+// #CHALLENGE
+
+//twitter challenge
+// PROMPT task: you have written 182 characters , you have -42 characters left
+
+var tweet=prompt("write any tweet");
+var tweetcount=tweet.length;
+
+alert("you have written "+tweetcount +"characters , you have"+(140-tweetcount) +"characters left");
+
+
+
 
