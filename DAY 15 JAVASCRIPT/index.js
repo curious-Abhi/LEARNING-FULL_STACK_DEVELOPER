c38b61d988c7ad15ea8852c59972d68313311703
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-03-26 12:31:27 +0530

    files updated

---
+++ DAY 15 JAVASCRIPT/index.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+console.log("Welcome to my github repo.")
+console.log("I like pizza!");
+
+window.alert("This is an alert!");
+window.alert("I like pizza!");
+
+//this is an comment
+
+/* this 
+is 
+also 
+a 
+comment */
+
