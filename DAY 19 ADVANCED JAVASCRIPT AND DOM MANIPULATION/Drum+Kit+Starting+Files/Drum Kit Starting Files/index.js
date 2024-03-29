2f1abcb2178fdbde6b6a43624b12b9957b3fe763
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-03-29 21:58:33 +0530

    files updated

--- DAY 19 ADVANCED JAVASCRIPT AND DOM MANIPULATION/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ DAY 19 ADVANCED JAVASCRIPT AND DOM MANIPULATION/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,5 +1,56 @@
-document.querySelector("button").addEventListener("click", handleclick)
+/*
 
-function handleclick(){
-    alert("i m clicked!")
+document.querySelectorAll("button")[0].addEventListener("click", handleclick);
+document.querySelectorAll("button")[1].addEventListener("click", handleclick);
+document.querySelectorAll("button")[2].addEventListener("click", handleclick);
+document.querySelectorAll("button")[3].addEventListener("click", handleclick);
+document.querySelectorAll("button")[4].addEventListener("click", handleclick);
+document.querySelectorAll("button")[5].addEventListener("click", handleclick);
+document.querySelectorAll("button")[6].addEventListener("click", handleclick);
+
+*/
+
+// one line using for loop
+
+//for(let i=0 i<document.querySelectorAll(".drum").lenght;i++)
+
+let numofdrum = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numofdrum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHTML; 
+    switch (buttonInnerHtml) {
+      case "w":
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+        break;
+      default:
+        console.log(buttonInnerHtml);
+        break;
+    }
+  });
 }
