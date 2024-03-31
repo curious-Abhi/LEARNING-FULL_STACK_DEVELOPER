ccbe5270e4ced6d8e5175dfc9eccdd229717651b
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-03-31 21:26:38 +0530

    files updated

---
+++ DAY 22 NODE JS BACKEND/2.3 NPM/index.js
@@ -0,0 +1,15 @@
+//var generateName = require('sillyname');
+/*
+
+import generateName from "sillyName";
+var sillyName = generateName();
+
+console.log(`my name is ${sillyName}`);
+*/
+
+//CHALLENGE 
+
+import superheroes from "superheroes"
+
+var supern=superheroes.random();
+console.log(`i am ${supern}`);
