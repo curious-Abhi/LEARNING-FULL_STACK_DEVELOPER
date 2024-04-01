eb81f3375c2e0374c66848e1c6e45a97df25a8d0
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-01 22:48:10 +0530

    files updated

--- DAY 23 EXPRESS JS WITH NODE JS/3.4 Middleware/index2.js
+++ DAY 23 EXPRESS JS WITH NODE JS/3.4 Middleware/index2.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use(morgan("tiny"))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
