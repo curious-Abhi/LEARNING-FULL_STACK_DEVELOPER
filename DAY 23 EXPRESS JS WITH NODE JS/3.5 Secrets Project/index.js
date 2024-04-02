b4c77cb2ca2d9b78c4a2369d598f41ab19846b19
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-02 16:12:12 +0530

    files updated

---
+++ DAY 23 EXPRESS JS WITH NODE JS/3.5 Secrets Project/index.js
@@ -0,0 +1,36 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+
+import express from "express"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app=express();
+const port=3000;
+  
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  function passchecker(req , res , next){
+     
+  }
+
+
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+
+
+  app.post("/submit",(req , res) =>{
+    console.log(req.body);
+  })
+
+app.listen(port,() =>{
+    console.log(`server running on the  port ${port}`);
+})
