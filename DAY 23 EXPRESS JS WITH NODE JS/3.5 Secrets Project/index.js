9c7a96df30c4525ea688cdb724b895d6e8373475
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-02 16:40:14 +0530

    files updated

--- DAY 23 EXPRESS JS WITH NODE JS/3.5 Secrets Project/index.js
+++ DAY 23 EXPRESS JS WITH NODE JS/3.5 Secrets Project/index.js
@@ -3,34 +3,42 @@
 //The password is ILoveProgramming
 
 
-import express from "express"
-import { dirname } from "path";
+import express from "express";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
 
-const app=express();
-const port=3000;
-  
-  app.use(bodyParser.urlencoded({extended: true}))
+let userIsAuthorised = false;
 
-  function passchecker(req , res , next){
-     
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
   }
+  next();
+}
 
+app.use(passwordCheck);
 
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
+});
 
-  app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(join(__dirname, "public", "secret.html"));
+  } else {
+    res.sendFile(join(__dirname, "public", "index.html"));
+    // Alternatively: res.redirect("/");
+  }
+});
 
-
-
-  app.post("/submit",(req , res) =>{
-    console.log(req.body);
-  })
-
-app.listen(port,() =>{
-    console.log(`server running on the  port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ DAY 23 EXPRESS JS WITH NODE JS/3.5 Secrets Project/readme.md
@@ -0,0 +1,41 @@
+# Password Protected Information Access
+
+## Overview
+
+This project demonstrates a simple password protection mechanism where accessing certain information is restricted behind a password prompt. Users need to enter the correct password to gain access to the protected information.
+
+## Prerequisites
+
+- Node.js and npm should be installed.
+
+## Installation
+
+Run the following command to install project dependencies:
+
+npm install
+
+
+## Project Structure
+
+- `/public`: Directory containing HTML files.
+- `server.js`: Main server file.
+
+## Running the Project
+
+To start the server, run:
+node index.js
+
+## Usage
+
+1. Access the application through a web browser.
+2. Enter the correct password to access the protected information.
+3. Password is ILoveProgramming.
+
+## Screenshots
+
+- [Screenshot 1: Login Page](public/login-page.png)
+- [Screenshot 2: Access Granted](public/access-granted.png)
+- [Screenshot 3: Access Denied](public/access-denied.png)
+
+
+
