fcca2e804a7050c10a47c82647b7f9845698cfb2
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-06 21:58:48 +0530

    files updated

--- DAY 27 PROJECT ANother BLOG WEB APPLICATION/routes/article.js
+++ DAY 27 PROJECT ANother BLOG WEB APPLICATION/routes/article.js
@@ -1,18 +1,18 @@
+
 import express from "express";
 const router = express.Router();
-
-
-router.get('/new', (req, res) => {
-  res.render('articles/new.ejs')
-})
 
 // Temporary storage for articles
 let articles = [];
 
+router.get('/new', (req, res) => {
+  res.render('articles/new.ejs');
+});
+
 // Route to create a new article
 router.post('/', (req, res) => {
-    const { title, description, markdown } = req.body;
-    const article = {
+    let { title, description, markdown } = req.body;
+    let article = {
         id: articles.length + 1, // Assign a unique ID
         title,
         description,
@@ -20,7 +20,19 @@ router.post('/', (req, res) => {
         createdAt: new Date()
     };
     articles.push(article); // Add the new article to the array
-    res.render('articles/show.ejs', { article });
+    res.render('articles/show.ejs', { article: article });
+});
+
+// Route to render the home page
+router.get('/', (req, res) => {
+  res.render('articles/index.ejs', { articles: articles });
+});
+
+// Route to render individual article page
+router.get('/show/:id', (req, res) => {
+  const id = req.params.id;
+  const article = articles.find(article => article.id === parseInt(id));
+  res.render('articles/show.ejs', { article: article });
 });
 
 export default router;
