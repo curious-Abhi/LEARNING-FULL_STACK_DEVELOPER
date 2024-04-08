6c190994cf01da36ea34392e2a9728c9688be945
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-08 19:19:29 +0530

    files updated

--- DAY 28 API/5.3 Axios/index.js
+++ DAY 28 API/5.3 Axios/index.js
@@ -25,7 +25,7 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
@@ -35,6 +35,23 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+
+  try{
+    console.log(req.body);
+    const type=req.body.type;
+    const participants=req.body.participants;
+    const response=await axios.get( `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+    const result=response.data;
+    console.log(result);
+    res.render("index.ejs",{
+      data:result[Math.floor(Math.random() * result.length)],
+    })
+  } catch(error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs",{
+      error:"no activities that match your criteria."
+    })
+  }
 });
 
 app.listen(port, () => {
