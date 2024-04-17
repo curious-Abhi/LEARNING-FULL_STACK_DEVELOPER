837e4736e66f68d487387af77e6f20cff6900cc6
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-17 19:34:01 +0530

    files updated

--- DAY 33 POSTGRESQL/33.8 PROJECT TO DO LIST/8.6 Permalist Project/index.js
+++ DAY 33 POSTGRESQL/33.8 PROJECT TO DO LIST/8.6 Permalist Project/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db=new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "abhishek",
+  port: 5432,
+})
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,22 +22,54 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/",async(req,res)=>{
+  try{
+      const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+      items=result.rows;
+    var options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    var today = new Date();
+    let currentday = today.toLocaleDateString("en-US", options);
+
+      res.render("index.ejs",{
+        listTitle:currentday,
+        listItems:items,
+      });
+  }catch(err){
+  console.log(err);
+  }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  //items.push({ title: item });
+  try{
+  await db.query("INSERT INTO items (title) VALUES ($1)",[item]);
   res.redirect("/");
+}catch(err){
+console.log(err);
+}
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async(req, res) => {
+  const item=req.body.updatedItemTitle;
+  const id=req.body.updatedItemId;
+  try{
+     await db.query("UPDATE items SET title=($1) WHERE id = $2",[item,id])
+  }catch(err){
+   console.log(err);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async(req, res) => {
+  const id=req.body.deleteItemId;
+  try{
+    await db.query("DELETE FROM items WHERE id=$1 ",[id]);
+    res.redirect("/");
+  }catch(err){
+   console.log(err);
+  }
+ 
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ DAY 33 POSTGRESQL/33.8 PROJECT TO DO LIST/readme.md
@@ -0,0 +1,13 @@
+# Permalist
+
+Permalist is a simple web application built with Node.js, Express, and PostgreSQL that allows users to create and manage a list of items with persistent storage.
+
+![Permalist Screenshot](screenshot.png)
+
+## Features
+
+- Add new items to your list.
+- Edit existing items.
+- Delete items from the list.
+- User-friendly interface for easy interaction.
+
