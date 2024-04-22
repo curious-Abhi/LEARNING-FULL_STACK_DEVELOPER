8d2650e0a4b462b4c2cae64280573647a43fd07a
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-22 17:55:19 +0530

    files updated

--- DAY 35 Authentication and Security/04 Managing Cookies and Sessions/9.3+Sessions+and+Cookies/index.js
+++ DAY 35 Authentication and Security/04 Managing Cookies and Sessions/9.3+Sessions+and+Cookies/index.js
@@ -5,19 +5,24 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(session({
-  resave:false,
-  secret:"TOPSECRETS",
-  saveUninitialized:true
-}));
+app.use(
+  session({
+    resave: false,
+    secret: "TOPSECRETS",
+    saveUninitialized: true,
+  
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,15 +48,24 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.get("/secrets",(req,res)=>{
-  if(req.isAuthenticated){
-   res.render("secrets.ejs")
-  }else{
-    res.redirect('/login')
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+
+app.get("/secrets", (req, res) => {
+  console.log(req.user);
+  if (req.isAuthenticated) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
   }
-})
-
-
+});
 
 app.post(
   "/login",
@@ -79,11 +93,15 @@ app.post("/register", async (req, res) => {
           console.error("Error hashing password:", err);
         } else {
           console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+         const result=  await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) Returning * ",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          const user=result.rows[0];
+          req.login(user,(err)=>{
+            console.log("success");
+            res.redirect("/secrets");
+          });
         }
       });
     }
@@ -92,38 +110,35 @@ app.post("/register", async (req, res) => {
   }
 });
 
-
-passport.use(new Strategy  ( async function verify(username,password,cb){
-
-
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      username,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedHashedPassword = user.password;
-      bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-        if (err) {
-          console.error("Error comparing passwords:", err);
-          return cb(err)
-        } else {
-          if (valid) {
-            return cb(null,user)
-        
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            console.error("Error comparing passwords:", err);
+            return cb(err);
           } else {
-            return cb(null,false)
+            if (valid) {
+              return cb(null, user);
+            } else {
+              return cb(null, false);
+            }
           }
-        }
-      });
-    } else {
-      return cb ("User not found");
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
-  
-}));
+  })
+);
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
