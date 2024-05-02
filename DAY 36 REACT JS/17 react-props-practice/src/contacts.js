46ddd422fd350483e5171345aed14728e20c5ecd
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-05-02 18:07:24 +0530

    files updated

---
+++ DAY 36 REACT JS/17 react-props-practice/src/contacts.js
@@ -0,0 +1,30 @@
+const contacts = [
+  {
+    name: "Curious Abhi",
+    imgURL: "https://avatars.githubusercontent.com/u/139208472?v=4",
+    phone: "+91 9523605579",
+    email: "abhishekk67781@gmail.com",
+  },
+  {
+    name: "Ms Dhoni",
+    imgURL:
+      "https://static.toiimg.com/thumb/msid-75607775,width-400,resizemode-4/75607775.jpg",
+    phone: "+123 456 789",
+    email: "thala@mahi.com",
+  },
+  {
+    name: "Ajay Devgan",
+    imgURL:
+      "https://i.pinimg.com/474x/5b/18/96/5b18968216179525ad3a8004ea9e13bf.jpg",
+    phone: "+987 654 321",
+    email: "ajay@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+export default contacts;
