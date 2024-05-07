bc5dcbd559b18ba39c999c467ec2af73220ad723
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-05-07 17:15:12 +0530

    files updated

---
+++ DAY 36 REACT JS/26 usestate-hook/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+//import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
