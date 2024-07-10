78ed5324bb2ea08f75aaaab30990a9e0415c4d1a
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-07-10 18:56:18 +0530

    files updated

---
+++ DAY 37 SIMPLE FULL STACK BLOG APPLICATIONS/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Aboutpage from "./pages/Aboutpage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<Aboutpage />} />
+            <Route path="/articles" element={<ArticlesList />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
